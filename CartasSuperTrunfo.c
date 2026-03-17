4a0ccc871eee03bd2e4e1366cbad61365ce262d9
deyvisson892221-svg <deyvisson892221@gmail.com>
2026-03-17 22:08:33 +0000

    HOJE FIZ ALTERACOES NO MEU DESAFIO. ELE MOSTRA QUAL CARTA VENCE USANDO IF OU ELSE

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,108 +1,78 @@
-// JOGO SUPER TRUNFO - CADASTRO DE CARTAS
-// Aluno Deivisson Marques
-//Linguagem C
-//Descricao: Programa que cadastra duas cartas de cidades
-//e exibe suas informacoes no terminal.
-//===========================================================
+// DESAFIO SUPER TRUNFO - NÍVEL NOVATO
+// Descricao: Comparação de cartas sem entrada do usuário
+// Aluno: Deivisson Marques
 
 #include <stdio.h> //biblioteca padrao para entrada e saida de dados
 
 int main() {
 
-  //=================================
   // DECLARACAO DAS VARIAVEIS CARTA 1 
-  //=================================
+  char estado1[] = "CE";                         // armazena a letra do estado A a H
+  char codigo1[] = "A01";                // codigo da carta ex: A01
+  char cidade1[] = "Fortaleza";             // nome da cidade
+  int populacao1 = "12300000";                     // populacao da cidade
+  float area1 = 1521.11;                         // area da cidade em km²
+  float pib1 = 699.28;                           // pib da cidade
+  int pontosturisticos1 = 50;                // numero de pontos turisticos
 
-  char estado1;                         // armazena a letra do estado A a H
-  char codigo_carta1[4];                // codigo da carta ex: A01
-  char nome_da_cidade1[50];             // nome da cidade
-  int populacao1;                       // populacao da cidade
-  float area1;                          // area da cidade em km²
-  float pib1;                           // pib da cidade
-  int pontosturisticos1;                // numero de pontos turisticos
-
-  //=================================
   // DECLARACAO DAS VARIAVEIS CARTA 2
-  //=================================
+  char estado2[] = "RJ";
+  char codigo2[] = "A02";
+  char cidade2[] = "Rio de Janeiro";
+  int populacao2 = 6000000;
+  float area2 = 1882.90;
+  float pib2 = 300.50;
+  int pontosturisticos2 = 35;
 
-  char estado2;
-  char codigo_carta2[4];
-  char nome_da_cidade2[50];
-  int populacao2;
-  float area2;
-  float pib2;
-  int pontosturisticos2;
+  // ===== VARIÁVEIS CALCULADAS =====
+    float densidade1, densidade2;
+    float pibPerCapita1, pibPerCapita2;
 
-  printf("===cadastro da carta1===\n");
+    // ===== CALCULOS =====
 
-  printf("estado1:(A a H): ");           // saída
-  scanf(" %c" , &estado1);               // entrada - Le um caractere
+    // Densidade populacional
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
 
-  printf("codigo da carta1: (ex A01): ");
-  scanf(" %s", codigo_carta1);           // le uma string
+    // PIB per capita
+    pibPerCapita1 = pib1 / populacao1;
+    pibPerCapita2 = pib2 / populacao2;
 
-  printf("nome da cidade: ");
-  scanf(" %[^\n]", nome_da_cidade1);     // le texto com espacos
-  getchar();                             // Limpa o buffer do teclado
+    // ===== EXIBIÇÃO DAS CARTAS =====
 
-  printf("populacao: ");
-  scanf(" %d", &populacao1);
+    printf("=== CARTA 1 ===\n");
+    printf("Cidade: %s (%s)\n", cidade1, estado1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontosturisticos1);
+    printf("Densidade Populacional: %.2f\n", densidade1);
+    printf("PIB per capita: %.6f\n", pibPerCapita1);
 
-  printf("area: (em km2): ");
-  scanf(" %f", &area1);
+    printf("\n=== CARTA 2 ===\n");
+    printf("Cidade: %s (%s)\n", cidade2, estado2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontosturisticos2);
+    printf("Densidade Populacional: %.2f\n", densidade2);
+    printf("PIB per capita: %.6f\n", pibPerCapita2);
 
-  printf(" Pib em Bilhoes: ");
-  scanf(" %f", &pib1);
+    // ===== COMPARAÇÃO =====
+    // ATRIBUTO ESCOLHIDO: POPULAÇÃO
 
-  printf("pontos turisticos: ");
-  scanf(" %d", &pontosturisticos1);
+    printf("\n=== Comparacao de Cartas (Atributo: Populacao) ===\n");
 
-  // ====== CARTA 2 =======
+    printf("Carta 1 - %s (%s): %d\n", cidade1, estado1, populacao1);
+    printf("Carta 2 - %s (%s): %d\n", cidade2, estado2, populacao2);
 
-  printf("=====cadastro da carta 2=====\n");
-
-  printf("estado (A a H): ");
-  scanf("%c", &estado2);
-
-  printf("codigo da carta (ex: B02): ");
-  scanf(" %s", codigo_carta2);
-
-  printf("nome da cidade: ");
-  scanf(" %[^\n]", nome_da_cidade2);
-  getchar(); //limpa o buffer do teclado
-
-  printf("populacao: ");
-  scanf(" %d", &populacao2);
-
-  printf("area (em km2): ");
-  scanf(" %f", &area2);
-
-  printf("pib, (em bilhoes): ");
-  scanf(" %f", &pib2);
-
-  printf("pontos turisticos: ");
-  scanf(" %d", &pontosturisticos2);
-
-  //======EXIBICAO DOS DADOS DAS CARTAS========//
-
-   printf("\n\n=== CARTA 1 ===\n");
-    printf("estado: %c\n", estado1);
-    printf("codigo: %s\n", codigo_carta1);
-    printf("nome da cidade: %s\n", nome_da_cidade1);
-    printf("populacao: %d\n", populacao1);
-    printf("area: %.2f km2\n", area1);
-    printf("pib: R$ %.2f bilhoes\n", pib1);
-    printf("numero depontos turisticos: %d\n", pontosturisticos1);
-
-    printf("\n\n=== CARTA 2 ===\n");
-    printf("estado: %c\n", estado2);
-    printf("codigo: %s\n", codigo_carta2);
-    printf("nome da cidade: %s\n", nome_da_cidade2);
-    printf("populacao: %d\n", populacao2);
-    printf("area: %.2f km2\n", area2);
-    printf("pib: R$ %.2f bilhoes\n", pib2);
-    printf("numero depontos turisticos: %d\n", pontosturisticos2);
-
+    // Estrutura de decisão para determinar a carta vencedora
+    if (populacao1 > populacao2) {
+        printf("\nResultado: Carta 1 (%s) venceu!\n", cidade1);
+    }
+    else if (populacao2 > populacao1) {
+        printf("\nResultado: Carta 2 (%s) venceu!\n", cidade2);
+    }
 
 
     return 0;                          // Finaliza o programa
